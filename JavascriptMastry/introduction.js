d98c9e97037fb0e072648794a650159773af404f
Akash <akashkumarsingh11032001@gmail.com>
2023-01-22 05:02:25 +0530

    datatype

--- JavascriptMastry/introduction.js
+++ JavascriptMastry/introduction.js
@@ -1,4 +1,6 @@
 /* variable in js */
+// -----------------
+
 // 1. var  keyword -> entier file scope
 var x = "Hello!"
 console.log(x)
@@ -8,3 +10,14 @@ console.log(y)
 // 3. const keyword -> value can't be changed
 const z = "Hey!"
 console.log(z)
+
+/* Datatype in js */
+// -----------------
+
+// 1. string 
+// 2. number
+// 3. NULL -> variable defined but value not declared
+// 4. undefiend -> neither value defined nor variable
+// 5. object
+// 6. sysmbol
+
