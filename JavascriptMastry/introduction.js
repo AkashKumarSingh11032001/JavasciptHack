63b9ab8e75fe8713bf8b05c347bf79209aada341
Akash <akashkumarsingh11032001@gmail.com>
2023-01-22 04:59:53 +0530

    variable

---
+++ JavascriptMastry/introduction.js
@@ -0,0 +1,10 @@
+/* variable in js */
+// 1. var  keyword -> entier file scope
+var x = "Hello!"
+console.log(x)
+// 2. let keyword -> defined function scope
+let y = "Hi!"
+console.log(y)
+// 3. const keyword -> value can't be changed
+const z = "Hey!"
+console.log(z)
