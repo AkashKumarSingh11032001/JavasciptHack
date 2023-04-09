dc8e0ae7691d2a90e1fcb8dd3e56391720a578ce
Akash <akashkumarsingh11032001@gmail.com>
2023-04-09 10:58:47 +0530

    scope

---
+++ JavascriptMastry/scope.js
@@ -0,0 +1,30 @@
+// // global scope
+// let name = "Akash";
+
+// const fun = () => {
+//   console.log(name);
+//   name = "Sweety"
+// };
+
+// const fun2 = () =>{
+//     console.log(name)
+// }
+
+// fun();
+// fun2();
+
+// // local / function scope
+// const localScope = () => {
+//     var a = "abc";
+//     console.log(a);
+
+//     const anotherFunction = () =>{
+//         a = "cde"
+//         var x2 = 3
+//     }
+//     anotherFunction()
+//     console.log(a);
+//     console.log(x2);
+// }
+// // console.log(a)
+// localScope()
---
+++ JavascriptMastry/trickyConcepts.js
@@ -0,0 +1,11 @@
+//  Scope : -
+
+const name = "Akash"
+var num = 5
+var num2 = 6
+let x = num1 + num2
+
+
+const fun = () => {
+    console.log(name)
+}
