b96a40afc7f5e044b31fa3fe6dc7858210432101
Akash <akashkumarsingh11032001@gmail.com>
2023-04-09 11:24:09 +0530

    strings

---
+++ JavascriptMastry/strings.js
@@ -0,0 +1,13 @@
+// :- Strings -:
+
+const single = "1";
+const double = "2";
+const backtick = `${1 + 2}`;
+
+// single & doouble are same but backtick provide extented functionaality
+
+console.log(single, double, backtick);
+
+const sum = (a, b) => a + b;
+
+console.log(`sum of two number ${sum(5, 4)}`);
